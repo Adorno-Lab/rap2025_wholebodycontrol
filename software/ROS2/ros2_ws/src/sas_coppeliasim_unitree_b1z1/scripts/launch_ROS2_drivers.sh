9db5da208382f2ee9fee8c45a44f0986f439b017
Juancho <juanjqogm@gmail.com>
2025-06-30 17:14:43 +0100

    Updated the bash script

--- software/ROS2/ros2_ws/src/sas_coppeliasim_unitree_b1z1/scripts/launch_ROS2_drivers.sh
+++ software/ROS2/ros2_ws/src/sas_coppeliasim_unitree_b1z1/scripts/launch_ROS2_drivers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/ros2_ws/
-colcon build
-cd ~/ros2_ws/ && source install/setup.bash
+
+cd ~/ros2_ws/ && colcon build
+source ~/ros2_ws/install/setup.bash 
 ros2 launch sas_coppeliasim_unitree_b1z1 sas_coppeliasim_unitree_b1z1_launch.py
