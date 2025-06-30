4bcd8126e8d38868de8fe48aed0885307be67059
Juancho <juanjqogm@gmail.com>
2025-06-30 17:32:56 +0100

    updated the dockerfile to fix the launcher

--- software/ROS2/ros2_ws/src/sas_coppeliasim_unitree_b1z1/scripts/launch_scene_and_ROS2_drivers.sh
+++ software/ROS2/ros2_ws/src/sas_coppeliasim_unitree_b1z1/scripts/launch_scene_and_ROS2_drivers.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+
+cd ~/git/rap2025_wholebodycontrol/software/ROS2/ros2_ws/src/sas_coppeliasim_unitree_b1z1/scripts
+~/utils/CoppeliaSim_Edu_V4_7_0_rev4_Ubuntu24_04/coppeliaSim.sh -s 0 -f ~/git/rap2025_wholebodycontrol/coppeliasim_scenes/B1_with_Z1_showcase.ttt &
+
 cd ~/git/rap2025_wholebodycontrol/software/ROS2/ros2_ws/src/sas_coppeliasim_unitree_b1z1/scripts
 ./launch_ROS2_drivers.sh
 
 
-cd ~/git/rap2025_wholebodycontrol/software/ROS2/ros2_ws/src/sas_coppeliasim_unitree_b1z1/scripts
-./launch_scene.sh
