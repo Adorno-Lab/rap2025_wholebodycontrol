1fb74f22063e8ebc64c794313420d27e8ce82e5c
Juancho <juanjqogm@gmail.com>
2025-06-30 16:29:50 +0100

    Added more scripts to launch the cs simulation and the ros2 package

---
+++ software/ROS2/ros2_ws/src/sas_coppeliasim_unitree_b1z1/scripts/launch_ROS2_drivers.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ~/ros2_ws/
+source install/setup.bash 
+ros2 launch sas_coppeliasim_unitree_b1z1 sas_coppeliasim_unitree_b1z1_launch.py
---
+++ software/ROS2/ros2_ws/src/sas_coppeliasim_unitree_b1z1/scripts/launch_scene_and_ROS2_drivers.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ~/git/rap2025_wholebodycontrol/software/ROS2/ros2_ws/src/sas_coppeliasim_unitree_b1z1/scripts
+sh launch_ROS2_drivers.sh
