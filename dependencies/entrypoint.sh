10c069587e586ce1c5bb229550a0f6a108f22487
Juancho <juanjqogm@gmail.com>
2025-07-01 17:20:14 +0100

    Modified the entrypoint.sh file aiming to fix it

--- dependencies/entrypoint.sh
+++ dependencies/entrypoint.sh
@@ -5,6 +5,5 @@
 #source /opt/ros/jazzy/setup.bash
 #cd ~/ros2_ws/ && colcon build
 #source ~/ros2_ws/install/setup.bash
-#source ~/.bashrc
-#ros2 launch sas_robot_driver_unitree_b1 real_b1z1_robot_launch.py 
-launch_ROS2_drivers
+source ~/.bashrc
+ros2 launch sas_robot_driver_unitree_b1 real_b1z1_robot_launch.py 
