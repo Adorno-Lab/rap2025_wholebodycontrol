3d0003bdaf5ec69bee2a222f32205a05a1e7009c
Juancho <juanjqogm@gmail.com>
2025-07-01 15:54:24 +0100

    added a new entrypoint bash file.

--- dependencies/install_sas_drivers_b1z1.sh
+++ dependencies/install_sas_drivers_b1z1.sh
@@ -18,7 +18,3 @@ echo "# Update the environment variable LIBRARY_PATH as instructed in https://ro
 echo "export LIBRARY_PATH=$LIBRARY_PATH:~/ros2_ws/src/sas_robot_driver_unitree_z1/src/z1_sdk/lib/:~/ros2_ws/src/sas_robot_driver_unitree_b1/src/unitree_legged_sdk/lib/cpp/amd64/" >> ~/.bashrc
 
 echo "alias launch_ROS2_drivers='cdros2 && source install/setup.bash && ros2 launch sas_robot_driver_unitree_b1 real_b1z1_robot_launch.py ' " >> ~/.bashrc 
-
-source /opt/ros/jazzy/setup.bash
-cd ~/ros2_ws/ && colcon build
-source ~/ros2_ws/install/setup.bash
