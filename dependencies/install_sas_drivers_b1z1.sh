040242a754bfbdef49358fde3abd003c3e861253
Juancho <juanjqogm@gmail.com>
2025-07-01 17:18:04 +0100

    updated the dockerfile to use the entrypoint

--- dependencies/install_sas_drivers_b1z1.sh
+++ dependencies/install_sas_drivers_b1z1.sh
@@ -11,6 +11,12 @@ cd ~/ros2_ws/src && git clone https://github.com/Adorno-Lab/sas_robot_driver_uni
 # Clone the sas_robot_driver_unitree_b1
 cd ~/ros2_ws/src && git clone https://github.com/Adorno-Lab/sas_robot_driver_unitree_b1 --recursive
 
+
+echo "source /opt/ros/jazzy/setup.bash" >> /etc/bash_env
+echo "# Source the ROS2 overlay, as instructed in https://github.com/SmartArmStack/smart_arm_stack_ROS2" >> /etc/bash_env
+echo "alias cdros2='cd ~/ros2_ws/' " >> /etc/bash_env
+echo "alias buildros2='cdros2 && colcon build && source install/setup.bash' " >> /etc/bash_env
+
 echo "# Update the environment variable LD_LIBRARY_PATH as instructed in https://ros2-tutorial.readthedocs.io" >> /etc/bash_env
 echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/ros2_ws/src/sas_robot_driver_unitree_z1/src/z1_sdk/lib/:~/ros2_ws/src/sas_robot_driver_unitree_b1/src/unitree_legged_sdk/lib/cpp/amd64/" >> /etc/bash_env
 
