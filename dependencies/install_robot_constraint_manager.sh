5b7c5016004deb9ae0ac8658a8c1884e5cb3bf8b
Juancho <juanjqogm@gmail.com>
2025-07-04 10:38:17 +0100

    Added a new bash file to install robot constraint manager

---
+++ dependencies/install_robot_constraint_manager.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p ~/git 
+
+# Clone the robot_constraint_manager package
+cd ~/git && git git clone https://github.com/Adorno-Lab/robot_constraint_manager
+cd ~/git/robot_constraint_manager
+mkdir -p build && cd build
+cmake ..
+make
+make install
+ldconfig
