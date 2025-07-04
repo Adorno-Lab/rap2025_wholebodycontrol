171139296c35ad43ce0a36e7b194d060cd6e768d
Juancho <juanjqogm@gmail.com>
2025-07-04 11:09:04 +0100

    Fixed bash file used to install rcmanager

--- dependencies/install_robot_constraint_manager.sh
+++ dependencies/install_robot_constraint_manager.sh
@@ -5,8 +5,6 @@ mkdir -p ~/git
 # Clone the robot_constraint_manager package
 cd ~/git && git git clone https://github.com/Adorno-Lab/robot_constraint_manager
 cd ~/git/robot_constraint_manager
-mkdir -p build && cd build
-cmake ..
-make
-make install
+mkdir -p build 
+cd ~/git/robot_constraint_manager/build && cmake .. && make && make install
 ldconfig
