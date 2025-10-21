f34458e38729a524b35604b3722609b7d41a7a27
Juancho <juanjqogm@gmail.com>
2025-10-21 17:10:21 +0100

    updated the Docker file to comply the modifications of the launch files

--- software/ROS2/ros2_ws/src/sas_coppeliasim_unitree_b1z1/scripts/launch_ROS2_drivers.sh
+++ software/ROS2/ros2_ws/src/sas_coppeliasim_unitree_b1z1/scripts/launch_ROS2_drivers.sh
@@ -3,4 +3,4 @@
 
 cd ~/ros2_ws/ && colcon build
 source ~/ros2_ws/install/setup.bash 
-ros2 launch sas_coppeliasim_unitree_b1z1 sas_coppeliasim_unitree_b1z1_launch.py
+ros2 launch sas_coppeliasim_unitree_b1z1 sas_coppeliasim_unitree_b1z1_white_launch.py
